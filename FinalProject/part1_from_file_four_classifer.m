45a9d4806c37994796ffec511f040dc62971a34c
arashparnia@gmail.com <Ar45hJ00n>
2017-03-24 11:07:14 +0100

    changing vl-sift descriptor

--- FinalProject/part1_from_file_four_classifer.m
+++ FinalProject/part1_from_file_four_classifer.m
@@ -12,6 +12,7 @@ function part1_from_file_four_classifer
 %     
 % end
 % 
+
 load('H_airplanes');
 load('H_motorbikes');
 load('H_faces');
@@ -47,10 +48,15 @@ training_instance_matrix_cars = training_cars';
 
 
 % 5 -- L1-regularized L2-loss support vector classification\n"
-model_motorbikes  = train(training_label_vector_motorbikes, sparse(training_instance_matrix_motorbikes),'-s 5');
-model_airplanes = train(training_label_vector_airplanes, sparse(training_instance_matrix_airplanes),'-s 5');
-model_faces = train(training_label_vector_faces, sparse(training_instance_matrix_faces),'-s 5');
-model_cars = train(training_label_vector_cars, sparse(training_instance_matrix_cars),'-s 5');
+% model_motorbikes  = train(training_label_vector_motorbikes, sparse(training_instance_matrix_motorbikes),'-s 5');
+% model_airplanes = train(training_label_vector_airplanes, sparse(training_instance_matrix_airplanes),'-s 5');
+% model_faces = train(training_label_vector_faces, sparse(training_instance_matrix_faces),'-s 5');
+% model_cars = train(training_label_vector_cars, sparse(training_instance_matrix_cars),'-s 5');
+
+model_motorbikes  = train(training_label_vector_motorbikes, sparse(training_instance_matrix_motorbikes));
+model_airplanes = train(training_label_vector_airplanes, sparse(training_instance_matrix_airplanes));
+model_faces = train(training_label_vector_faces, sparse(training_instance_matrix_faces));
+model_cars = train(training_label_vector_cars, sparse(training_instance_matrix_cars));
 
 
 % % save('model','model');
@@ -64,10 +70,10 @@ load('H_test');
 test_mat = cell2mat(H_test);
 
 [~,s] = size(test_mat); 
-test_label_vector = ones(1,s);
-test_label_vector(1,50:100) = test_label_vector(1,50:100)* 4;
-test_label_vector(1,100:150) = test_label_vector(1,100:150)* 3;
-test_label_vector(1,150:200) = test_label_vector(1,150:200)* 2;
+test_label_vector = ones(1,s) * 0;
+test_label_vector(1,50:100) = test_label_vector(1,50:100)* 4 * 0;
+test_label_vector(1,100:150) = test_label_vector(1,100:150)* 3 * 0;
+test_label_vector(1,150:200) = test_label_vector(1,150:200)* 2 * 0;
 
 
 test_instance_matrix = test_mat;
@@ -77,9 +83,10 @@ test_label_vector = test_label_vector';
 test_instance_matrix = test_instance_matrix';
 
 
-[predict_label, accuracy, prob_estimates] = predict(test_label_vector, sparse(test_instance_matrix), model_motorbikes)
-[sortvals, sortidx] = sort(prob_estimates,'descend');
-sortidx(1:10)
+% [predict_label, accuracy, prob_estimates] = predict(test_label_vector, sparse(test_instance_matrix), model_motorbikes);
+% [sortvals, sortidx] = sort(prob_estimates,'descend');
+% sortidx(1:10)
+% 
 % [predict_label, accuracy, prob_estimates] = predict(test_label_vector, sparse(test_instance_matrix), model_airplanes)
 % 
 % [predict_label, accuracy, prob_estimates] = predict(test_label_vector, sparse(test_instance_matrix), model_faces)
